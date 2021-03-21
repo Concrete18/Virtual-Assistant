0083b5dd399e5321db647c6bef5c2ab3acaf6b36
Concrete18 <michaelericson19@gmail.com>
2021-03-20 22:54:35 -0400

    switched back to self due to pylance issues

--- Virtual_Assistant.py
+++ Virtual_Assistant.py
@@ -1,13 +1,16 @@
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.tokenize import word_tokenize
+from gtts import gTTS
+import playsound
+from threading import Thread
 import difflib
 import random
 import json
+import os
 from Assistant_Actions import Action
 
 
 class Assistant:
-
 	# module init
 	stemmer = LancasterStemmer()
 
@@ -25,123 +28,168 @@ class Assistant:
 		data = json.load(file)
 	assistant_name = data['settings']['assistant_name']
 	user_name = data['settings']['real_name']
+	synth_voice = data['settings']['synth_voice']
 	similarity_req = data['settings']['similarity_req']
 	debug = data['settings']['debug']
 
 
-	@classmethod
-	def simplify_phrase(cls, sentence) -> list:
-		sentence = cls.stemmer.stem(sentence.lower())
+	def simplify_phrase(self, sentence):
+		'''
+		ph
+		'''
+		sentence = self.stemmer.stem(sentence.lower())
 		word_tokens = word_tokenize(sentence)
-		return [word for word in word_tokens if not word in cls.stop_words]
+		return [word for word in word_tokens if not word in self.stop_words]
 
 
-	@classmethod
-	def phrase_matcher(cls, phrase) -> dict:
-		max_similarity = 0
-		prepped_phrase = cls.simplify_phrase(phrase)
+	def phrase_matcher(self, phrase) -> dict:
+		'''
+		Takes the given phrase and finds a match in intents.json.
+		'''
+		max_similarity = self.similarity_req
+		prepped_phrase = self.simplify_phrase(phrase)
 		match_dict = {'tag':None}
-		for item in cls.intents:
+		for item in self.intents:
 			for pattern in item['patterns']:
-				prepped_pattern = cls.simplify_phrase(pattern)
+				prepped_pattern = self.simplify_phrase(pattern)
 				similarity = difflib.SequenceMatcher(None, prepped_pattern, prepped_phrase).ratio()
 				# print(pattern, similarity)
-				if similarity > max_similarity and similarity > cls.similarity_req:
-					if cls.debug == 1:
+				if similarity > max_similarity:
+					if self.debug == 1:
 						print(f'\nPattern: {pattern}\n{similarity}')
 					max_similarity = similarity
 					match_dict = item
 					match_dict['pattern'] = pattern.lower()
-		if cls.debug == 1 and 'tag' in match_dict.keys():
+		if self.debug == 1 and 'tag' in match_dict.keys():
 			print(f'\nTag: {match_dict["tag"]}\nSimilarity:{max_similarity}')
 		return match_dict
 
 
-	@classmethod
-	def startup_convo(cls):
-		print(f"{cls.assistant_name}:\nHello {cls.user_name}, How can I help?")
+	def speak(self, text):
+		'''
+		Using gTTS, verbally says the text variable with Text-To-Speech Text-To-Speech
+        Arguments:
+        text -- text to be spoken using Google Text-To-Speech
+        '''
+		tts = gTTS(text=text, lang='en')
+		filename = 'voice.mp3'
+		if os.path.exists(filename):  # removes old file if it exists due to error before it is deleted
+			os.remove(filename)
+		try:
+			tts.save(filename)
+		except ValueError as error:
+			if self.debug == 1:
+				print(error)
+			return
+		playsound.playsound(filename)
+		os.remove(filename)
 
 
-	@classmethod
-	def respond(cls, response, use_input=0) -> str:
-		response = response.replace('{assistant_name}', cls.assistant_name)
-		response = response.replace('{user_name}', cls.user_name)
+	def startup_convo(self):
+		'''
+		ph
+		'''
+		response = f"Hello {self.user_name}, How can I help?"
+		if self.synth_voice:
+			Thread(target=self.speak, args=(response,)).start()
+		print(f'{self.assistant_name}:\n{response}')
+
+
+	def respond(self, response, use_input=0):
+		'''
+		ph
+		'''
+		response = response.replace('{assistant_name}', self.assistant_name)
+		response = response.replace('{user_name}', self.user_name)
+		if self.synth_voice:
+			Thread(target=self.speak, args=(response,)).start()
 		if use_input == 0:
-			print(f'\n{cls.assistant_name}:\n{response}')
+			print(f'\n{self.assistant_name}:\n{response}')
 		else:
-			return input(f'\n{cls.assistant_name}:\n{response}')
+			return input(f'\n{self.assistant_name}:\n{response}')
 
 
-	@classmethod
-	def run_loop(cls):
+	def run_loop(self):
 		'''
 		Starts assistant loop.
 		'''
-		cls.startup_convo()
-		while True:
-			print(f'\n{cls.user_name}:')
-			user_input = input()
-			if user_input == '':  # skips to beginning if no response is given
-				continue
-			# takes user_input and gets match information
-			match_dict = cls.phrase_matcher(user_input)
-			if match_dict['tag'] == None:
-				response = random.choice(cls.unknown_msg_resp)
-			elif 'responses' in match_dict.keys():
-				response = random.choice(match_dict['responses'])
-			# assistant actions
-			if 'action' in match_dict.keys():
-				# Hue lights and Smarthub Actions
-				if match_dict['action'] == 'turn_on_lights':
-					Action.Hue_Hub.run_scene('My Bedroom', 'Bright', 1)
+		self.startup_convo()
+		try:
+			while True:
+				print(f'\n{self.user_name}:')
+				user_input = input()
+				if user_input == '':  # skips to beginning if no response is given
+					continue
+				# takes user_input and gets match information
+				match_dict = self.phrase_matcher(user_input)
+				if match_dict['tag'] == None:
+					response = random.choice(self.unknown_msg_resp)
+				elif 'responses' in match_dict.keys():
+					response = random.choice(match_dict['responses'])
+				# assistant actions
+				if 'action' in match_dict.keys():
+					# voice
+					if 'talking' in match_dict['action']:
+							if match_dict['action'] == 'start_talking':
+								self.synth_voice = 1
+							elif match_dict['action'] == 'stop_talking':
+								self.synth_voice = 0
+					# Hue lights and Smarthub Actions
+					if match_dict['action'] == 'turn_on_lights':
+						Action.Hue_Hub.run_scene('My Bedroom', 'Bright', 1)
 
-				if match_dict['action'] == 'turn_on_backlight':
-					Action.Hue_Hub.run_scene('My Bedroom', 'Backlight', 1)
+					if match_dict['action'] == 'turn_on_backlight':
+						Action.Hue_Hub.run_scene('My Bedroom', 'Backlight', 1)
 
-				elif match_dict['action'] == 'turn_off_lights':
-					Action.Hue_Hub.set_group('My Bedroom', 'on', False)
+					elif match_dict['action'] == 'turn_off_lights':
+						Action.Hue_Hub.set_group('My Bedroom', 'on', False)
 
-				elif match_dict['action'] == 'toggle_heater':
-					response = Action.toggle_heater(match_dict['pattern'])
+					elif match_dict['action'] == 'toggle_heater':
+						response = Action.toggle_heater(match_dict['pattern'])
 
-				# Computer Control Actions
-				elif match_dict['action'] == 'open_folder':
-					folder = cls.respond('What folder do you want to open?', use_input=1)
-					Action.open_folder(folder)
+					# Computer Control Actions
+					elif match_dict['action'] == 'open_folder':
+						folder = self.respond('What folder do you want to open?', use_input=1)
+						Action.open_folder(folder)
 
-				elif match_dict['action'] == 'run_script':
-					script = cls.respond('What script do you want to run?', use_input=1)
-					Action.run_script(script)
+					elif match_dict['action'] == 'run_script':
+						script = self.respond('What script do you want to run?', use_input=1)
+						Action.run_script(script)
 
-				elif match_dict['action'] == 'set_audio_default':
-					Action.set_audio_default(match_dict['pattern'])
+					elif match_dict['action'] == 'set_audio_default':
+						Action.set_audio_default(match_dict['pattern'])
 
-				elif match_dict['action'] == 'display_switch':
-					Action.display_switch(match_dict['pattern'])
+					elif match_dict['action'] == 'display_switch':
+						Action.display_switch(match_dict['pattern'])
 
-				elif match_dict['action'] == 'start_vr':
-					Action.Start_vr()
+					elif match_dict['action'] == 'start_vr':
+						Action.Start_vr()
 
-				# Informational Actions
-				elif match_dict['action'] == 'date_time':
-					response = Action.check_time_date(match_dict['pattern'])
+					# Informational Actions
+					elif match_dict['action'] == 'date_time':
+						response = Action.check_time_date(match_dict['pattern'])
 
-				elif match_dict['action'] == 'time_till':
-					if 'subject' in match_dict.keys():
-						response = Action.time_till(
-							day=match_dict['day'],
-							month=match_dict['month'],
-							subject=match_dict['subject'])
-					else:
-						# TODO find date in pattern
-						print(match_dict['pattern'])
-						response = Action.time_till(match_dict['date'])
+					elif match_dict['action'] == 'what_is_tomorrow':
+						response = Action.check_time_date(match_dict['pattern'])
 
-				if match_dict['action'] == 'exit':
-					exit()
+					elif match_dict['action'] == 'time_till':
+						if 'subject' in match_dict.keys():
+							response = Action.time_till(
+								day=match_dict['day'],
+								month=match_dict['month'],
+								subject=match_dict['subject'])
+						else:
+							# TODO find date in pattern
+							response = Action.time_till_custom(match_dict['date'])
 
-			cls.respond(response)
+					if match_dict['action'] == 'exit':
+						self.respond(response)
+						exit()
+
+				self.respond(response)
+		except KeyboardInterrupt:
+			print(f'Good bye {self.user_name}')
 
 
 if __name__ == "__main__":
-	Assistant.run_loop()
+	Assistant().run_loop()
