fc0ad032f8e474856ac97549058145b2cfa56765
Concrete18 <michaelericson19@gmail.com>
2021-03-20 22:49:42 -0400

    switched back to self instead of cls due to pylance issues

--- Assistant_Actions.py
+++ Assistant_Actions.py
@@ -5,6 +5,7 @@ from time import sleep
 import datetime as dt
 import subprocess
 import sys
+import re
 import os
 
 
@@ -21,35 +22,29 @@ class Action:
     ahk_tv = 'Run nircmd setdefaultsounddevice "SONY TV" 1'
 
 
-    @classmethod
-    def time_till(cls, month, day, year=None, subject=None,) -> str:
-        '''
-        Gives the time till the given date arrives.
-        '''
-        if year == None:
-            year = dt.datetime.now().year
-        # time_till = dt.datetime(month=month, day=day, year=year) - dt.datetime.now()
-        time_till = dt.datetime.strptime(f'{month}-{day}-{year}', f'%m-%d-%Y') - dt.datetime.now()
-        if subject != None:
-            return f'{subject} is in {time_till.days} days.'
-        else:
-            return f'{time_till.days} days till {month}/{day}/{year}.'
-
-
-    @classmethod
-    def open_folder(cls, folder_dir):
+    @staticmethod
+    def open_folder(folder):
         '''
         Opens folder given as folder_dir.
         '''
-        subprocess.Popen(rf'explorer /select, {folder_dir}')
-        # TODO add open_folder function
+        # TODO finish open_folder function
+        folders = {
+            'Home Control Interface':r'D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Home Control Interface.lnk',
+            'Timed Sleep or Shutdown':r'D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Timed Sleep or Shutdown.lnk',
+            'Media Release Updater':r'D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Media Release Updater.lnk'
+        }
+        for folder_name, folder_dir in folders.items():
+            if folder in folder_name:
+                subprocess.Popen(rf'explorer /select, {folder_dir}')
+                return
 
 
-    @classmethod
-    def run_script(cls, script):
+    @staticmethod
+    def run_script(script):
         '''
         Runs script based on given script name if it is in the scripts dictionary.
         '''
+        # TODO finish run_script function
         scripts = {
             'Home Control Interface':r'D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Home Control Interface.lnk',
             'Timed Sleep or Shutdown':r'D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Timed Sleep or Shutdown.lnk',
@@ -57,13 +52,12 @@ class Action:
         }
         for script_name, script_dir in scripts.items():
             if script in script_name:
-                subprocess.run([sys.executable, script], cwd=os.path.dirname(script_dir))
+                subprocess.run([sys.executable, f'{script}.lnk'], cwd=os.path.dirname(script_dir))
                 return
         print('No script found with that name.')
 
 
-    @classmethod
-    def display_switch(cls, pattern):
+    def display_switch(self, pattern):
         '''
         Switches display to the mode that matches the pattern argument. Works for PC and TV mode.
 
@@ -78,13 +72,12 @@ class Action:
         subprocess.call([f'{os.getcwd()}/Batches/{mode} Mode.bat'])  # runs .bat for different modes
         sleep(10)
         if mode == 'PC':  # switches audio default via an AHK wrapper
-            cls.ahk.run_script(cls.ahk_speakers, blocking=False)
+            self.ahk.run_script(self.ahk_speakers, blocking=False)
         else:
-            cls.ahk.run_script(cls.ahk_tv, blocking=False)
+            self.ahk.run_script(self.ahk_tv, blocking=False)
 
 
-    @classmethod
-    def set_audio_default(cls, pattern):
+    def set_audio_default(self, pattern):
         '''
         Sets the audio device depending on the device is mentioned in the pattern.
 
@@ -93,15 +86,14 @@ class Action:
         pattern -- matched response from Phrase_Matcher
         '''
         if 'pc' in pattern:
-            cls.ahk.run_script(cls.ahk_speakers, blocking=False)
+            self.ahk.run_script(self.ahk_speakers, blocking=False)
         elif 'tv' in pattern:
-            cls.ahk.run_script(cls.ahk_tv, blocking=False)
+            self.ahk.run_script(self.ahk_tv, blocking=False)
         else:
-            cls.ahk.run_script(cls.ahk_headphones, blocking=False)
+            self.ahk.run_script(self.ahk_headphones, blocking=False)
 
 
-    @classmethod
-    def toggle_heater(cls, pattern):
+    def toggle_heater(self, pattern):
         '''
         Turns the heater on or off depending on if ON or OFF is in the pattern.
 
@@ -110,38 +102,69 @@ class Action:
         pattern -- matched response from Phrase_Matcher
         '''
         if 'on' in pattern:
-            cls.Heater.turn_on()
+            self.Heater.turn_on()
             return 'Turned on the heater.'
         elif 'of' in pattern:
-            cls.Heater.turn_off()
+            self.Heater.turn_off()
             return 'Turned off the heater.'
 
 
-    @classmethod
-    def start_vr(cls):
+    def start_vr(self):
         '''
         Start VR Function.
         '''
         # runs SteamVR
         subprocess.call("D:/My Installed Games/Steam Games/steamapps/common/SteamVR/bin/win64/vrstartup.exe")
-        if cls.Lighthouse.get_sysinfo()["relay_state"] == 0:  # turns on Lighthouse if it is off
-            cls.Lighthouse.turn_on()
+        if self.Lighthouse.get_sysinfo()["relay_state"] == 0:  # turns on Lighthouse if it is off
+            self.Lighthouse.turn_on()
 
 
-    @classmethod
-    def check_time_date(cls, pattern) -> str:
+    @staticmethod
+    def check_time_date(pattern) -> str:
         '''
         Says the Date or time depending on which is in the pattern chosen.
 
         Arguments:
 
-        pattern -- matched response from Phrase_Matcher
+        pattern -- matched response from phrase_matcher
         '''
         if 'time' in pattern:
             current_time = dt.datetime.now().strftime("%I:%M %p")
-            print(current_time[0])
             if current_time[0] == '0':
                 current_time = current_time[1:]
             return f'It is {current_time}'
+        elif 'tomorrow' in pattern:
+            tomorrow = dt.datetime.today() + dt.timedelta(days=1)
+            return f"It is {tomorrow.strftime('%A, %B %d, %Y')}"
         elif 'date' or 'day' in pattern:
-            return f"Today's date is {dt.datetime.now().strftime('%A, %B %d, %Y')}"
+            return f"It is {dt.datetime.now().strftime('%A, %B %d, %Y')}"
+
+
+    @staticmethod
+    def time_till(month, day, year=None, subject=None,):
+        '''
+        Gives the time till the given date arrives.
+        '''
+        if year == None:
+            year = dt.datetime.now().year
+        # time_till = dt.datetime(month=month, day=day, year=year) - dt.datetime.now()
+        time_till = dt.datetime.strptime(f'{month}-{day}-{year}', f'%m-%d-%Y') - dt.datetime.now()
+        if subject != None:
+            return f'{subject} is in {time_till.days} days.'
+        else:
+            return f'{time_till.days} days till {month}/{day}/{year}.'
+
+
+    @staticmethod
+    def time_till_custom(pattern):
+        '''
+        Gives the time till the given date arrives.
+        '''
+        date_entry = re.search(rf'/d', pattern)
+        month, day, year = date_entry.split('-')
+        if year == None:
+            year = dt.datetime.now().year
+
+        time_till = dt.datetime.strptime(f'{month}-{day}-{year}', f'%m-%d-%Y') - dt.datetime.now()
+
+        return f'{time_till.days} days till {month}/{day}/{year}.'
