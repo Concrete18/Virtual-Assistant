415b2bf9fffc3dad96fdb23bdfa50d854c2474ea
Concrete18 <michaelericson19@gmail.com>
2021-03-20 22:48:41 -0400

    added more tests

--- test_Vitrual_Assistant.py
+++ test_Vitrual_Assistant.py
@@ -9,7 +9,8 @@ class Test_Virtual_Assistant(unittest.TestCase):
         '''
         Tests phrase_matcher to be sure it returns the proper tags.
         '''
-        Assistant.debug = 0
+        Test = Assistant()
+        Test.debug = 0
 
         test_strings = {
         'Can you turn on my lights please?':'turn_on_lights',
@@ -20,16 +21,21 @@ class Test_Virtual_Assistant(unittest.TestCase):
         'switch audio to my pc please?':'set_audio_default',
         'how are you doing today?':'ask_about_assistant',
         'thanks for all the help?':'appreciation',
+        'thanks.':'appreciation',
+        'thanks!':'appreciation',
         'Hello assistant':'greeting',
         'start vr':'start_vr',
         'what is todays date?':'date_time',
         'what time is it?':'date_time',
         'When is Christmas?':'time_till_christmas',
         'When is Thanksgiving':'time_till_thanksgiving',
-        'When is Thanksgivin':'time_till_thanksgiving',
+        'How many days till 12-10?':'time_till_custom',
+        'How many days till 2-8?':'time_till_custom',
+        "How long till March 8th":'time_till_custom',
+        'How many days till March 8th?':'time_till_custom',
         }
         for string, tag in test_strings.items():
-            self.assertEqual(Assistant.phrase_matcher(string)['tag'], tag)
+            self.assertEqual(Test.phrase_matcher(string)['tag'], tag)
 
 
 if __name__ == '__main__':
